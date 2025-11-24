519834fde8cb9639aa27b44e2e741a8f3ce9dd70
nobody <hugosum.dev@protonmail.com>
2025-11-24 14:19:04 +0000

    add new function

--- lua/syringe/init.lua
+++ lua/syringe/init.lua
@@ -4,4 +4,9 @@ function M.setup(opts)
 
 end
 
+function M.install()
+   local dirname = string.sub(debug.getinfo(1).source, 2, string.len('/init.lua') * -1) 
+   print("my dir is", dirname)
+end
+
 return M
