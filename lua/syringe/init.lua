cd30d19796d53142f8115d4d9197226b8d4e4b96
nobody <hugosum.dev@protonmail.com>
2025-11-24 16:55:14 +0000

    add gitignore

--- lua/syringe/init.lua
+++ lua/syringe/init.lua
@@ -1,12 +1,22 @@
 local M = {}
 
 function M.setup(opts)
-
+  M.opts = vim.tbl_deep_extend('force', {
+    host_languages = { 'javascript' },
+  }, opts or {})
 end
 
 function M.install()
-   local dirname = string.sub(debug.getinfo(1).source, 2, string.len('/init.lua') * -1) 
-   print("my dir is", dirname)
+  local plugin_root_dir =
+    debug.getinfo(1).source:sub(2, string.len('/lua/syringe/init.lua') * -1 - 1)
+
+  for _, language in ipairs(M.opts.host_languages) do
+    local injection_dir = vim.fs.joinpath(plugin_root_dir, 'queries', language)
+    vim.fn.mkdir(injection_dir, 'p')
+
+    local injection_file = vim.fs.joinpath(injection_dir, 'injections.scm')
+    vim.fn.writefile({ ";; extends" },injection_file)
+  end
 end
 
 return M
