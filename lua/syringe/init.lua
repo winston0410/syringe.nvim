a61aede64b63f4f98a61a3a1fc9860524d55ddb3
Hugo Sum <hsum@trintech.com>
2025-11-26 12:19:01 +0000

    fix lint

--- lua/syringe/init.lua
+++ lua/syringe/init.lua
@@ -216,7 +216,7 @@ end
 ---@param language string Treesitter parser name, which is not always the same with the value of filetype in Neovim for that language.
 function M.generate_injections(language)
   local commentstring = get_comment_string(language)
-  assert(type(commentstring) == "string", string.format("vim.bo.commentstring of filetype %s is not string", cs))
+  assert(type(commentstring) == "string", string.format("vim.bo.commentstring of filetype %s is not string", commentstring))
   -- REF https://stackoverflow.com/a/27455195
   local comment_symbol = string.format(commentstring, ""):gsub('^%s*(.-)%s*$', '%1')
 
@@ -234,7 +234,7 @@ function M.sync()
   local plugin_root_dir =
     debug.getinfo(1).source:sub(2, string.len('/lua/syringe/init.lua') * -1 - 1)
 
-  for language, rule in pairs(M.opts.rules) do
+  for language, _ in pairs(M.opts.rules) do
     local injection_dir = vim.fs.joinpath(plugin_root_dir, 'queries', language)
     vim.fn.delete(injection_dir, "rf")
     vim.fn.mkdir(injection_dir, 'p')
